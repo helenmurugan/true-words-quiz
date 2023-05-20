42547b995359f14031e426c25bb72c2940a5e432
Helen Murugan <gallonhelen@hotmail.com>
2023-05-20 20:37:20 +0000

    Add objects to options in questionBank

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,30 +1,51 @@
 
-let questionBank = [
+const questionBank = [
     {
       definition: "A fledgling pigeon about four weeks old",
-      options: ["Squab", "Squib", "Pigeot", "Pigchick"],
-      answer: 0
+      options: [
+        {text: "Squab", correct: true}
+        {text: "Squib", correct: false}
+        {text: "Pigeot", correct: false}
+        {text: "Pigchick", correct: false}
+      ],
     },
     {
       definition: "A word meaning confuse/perplex/fluster",
-      options: ["Bumfuzzle", "Dumduffer", "Fuzzpop", "Conperflu"],
-      answer: 0
-    },
-    {
-      definition: "The art of engraving or carving whale ivory",
-      options: ["Scrimshaw", "Whaleycarve", "Scriddle", "Blofrum"],
-      answer: 0
+      options: [
+        {text: "Squibblebum", correct: false}
+        {text: "Dumduffer", correct: false}
+        {text: "Fuzzpop", correct: false}
+        {text: "Bumfuzzle", correct: true}
+      ],
     },
     {
       definition: "A song or melody that keeps repeating in one’s mind",
-      options: ["Earworm", "Jinglebug", "Jazzfly", "Headnut"],
-      answer: 0
+      options: [
+        {text: "Jinglebug", correct: false}
+        {text: "Earworm ", correct: true}
+        {text: "Jazzfly", correct: false}
+        {text: "Headnut", correct: false}
+      ],
+    },
+    {
+      definition: "A noisy or overexcited reaction or response to something",
+      options: [
+        {text: "Brouhaha", correct: true}
+        {text: "Quibbleschnort", correct: false}
+        {text: "Hubbadub", correct: false}
+        {text: "Frazzleblip", correct: false}
+      ],
+    },
+    {
+      definition: "Endurance training where a runner alternates between sprinting and jogging",
+      options: ["Fartlek", "Sprintjog", "Jogsprint", "Lekfart"],
+      
     },
     {
       definition: "A lively discussion/argument/dispute",
       options: ["Argy-bargy", "Stifle-pifle", "Debby-toffy", "Squibstuff"],
       answer: 0
-    }
+    },
     {
       definition: "A sweet, aromatic wine made from green grapes native to North Carolina",
       options: ["Scuppernong", "Demderbe", "Sonomo", "Vars"],
@@ -49,7 +70,7 @@ let questionBank = [
       definition: "A lover of words",
       options: ["Logophile", "Wordophile", "Vocabularophile", "Lettophile"],
       answer: 0
-    }
+    },
     {
       definition: "To make something more lively or appealing by a small change",
       options: ["Zhush", "Frush", "Ploomp", "Prizzle"],
@@ -70,11 +91,6 @@ let questionBank = [
       options: ["Metrology", "Measury", "Metellogy", "Measology"],
       answer: 0
     },
-    {
-      definition: "Endurance training where a runner alternates between sprinting and jogging",
-      options: ["Fartlek", "Sprintjog", "Jogsprint", "Lekfart"],
-      answer: 0
-    }
     {
       definition: "Floating wreckage of a ship or its cargo",
       options: ["Flotsam", "Wrecksam", "Sealitter", "Flocean"],
@@ -99,7 +115,7 @@ let questionBank = [
       definition: "The act of finding something valuable when you are not looking for it",
       options: ["Serendipity", "Fruitipidity", "Joyhoy", "Zoobloop"],
       answer: 0
-    }
+    },
     {
       definition: "Language that is meaningless or made unintelligible by excessive use of technical terms",
       options: ["Gobbledygook", "Gookledegob", "Gookgobble", "Gobblefrobble"],
@@ -115,14 +131,9 @@ let questionBank = [
       options: ["Flibbertigibbet", "Zipperdibbet", "Flubberdub", "Glibberglook"],
       answer: 0
     },
-    {
-      definition: "A noisy or overexcited reaction or response to something",
-      options: ["Brouhaha", "Quibbleschnort", "Hubbadubdub", "Frazzleblip"],
-      answer: 0
-    },
-    {
-      definition: "A foolish person",
-      options: ["Nincompoop", "Fruzzleblip", "Blibberfloop", "Snickerdoodle"],
-      answer: 0
-    }
+  
   ];
+
+let definition = document.getElementById("definition");
+let wordChoices = Array.from(document.getElementsByClassName("word-choice"));
+console.log(wordChoices);
